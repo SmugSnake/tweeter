12848d862a4302f7587327a9b437b113fcb87351
Evan Kerr <evankrkerr90@gmail.com>
2018-06-19 19:43:20 +0000

    tweet layout mostly completed, need to add hover state

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,10 +1,12 @@
 $(document).ready(function () {
 
-    $("section.new-tweet textarea").keyup(function (event) {
+    $("section.new-tweet textarea").keyup(function() {
         var chars = 140 - $(this).val().length;
         $(this).parent().find(".counter").html(chars);
         if (chars < 0) {
             $(this).parent().find(".counter").addClass("red");
+        } else {
+            $(this).parent().find(".counter").removeClass("red")
         }
     });
 });
