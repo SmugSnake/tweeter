8a8be918a080c76ba042ba78c8beecf954f47006
Evan Kerr <evankrkerr90@gmail.com>
2018-06-20 19:49:04 +0000

    rendering tweets submited to serve

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,19 +1,18 @@
-$( document ).ready(function() {
+$(document).ready(function () {
 
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+  /*
+   * Client-side JS logic goes here
+   * jQuery is already loaded
+   * Reminder: Use (and do all your DOM work in) jQuery's document ready function
+   */
 
-const data = [
-    {
+  const data = [{
       "user": {
         "name": "Newton",
         "avatars": {
-          "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+          "small": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
           "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-          "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+          "large": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
         },
         "handle": "@SirIsaac"
       },
@@ -26,11 +25,12 @@ const data = [
       "user": {
         "name": "Descartes",
         "avatars": {
-          "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+          "small": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
           "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-          "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+          "large": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
         },
-        "handle": "@rd" },
+        "handle": "@rd"
+      },
       "content": {
         "text": "Je pense , donc je suis"
       },
@@ -40,9 +40,9 @@ const data = [
       "user": {
         "name": "Johann von Goethe",
         "avatars": {
-          "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+          "small": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
           "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-          "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+          "large": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
         },
         "handle": "@johann49"
       },
@@ -53,7 +53,7 @@ const data = [
     }
   ];
 
-function createTweetElement(tweet) {
+  function createTweetElement(tweet) {
     let $tweet = $('<article>').addClass('full tweet');
     var image = $('<img>').attr('src', tweet.user.avatars.small)
     var handle = $('<span>').text(tweet.user.handle).addClass('creator');
@@ -72,21 +72,46 @@ function createTweetElement(tweet) {
     $footer.append(createdDate, $iconSpan);
     $tweet.append($header, $content, $footer);
 
-    
+
     return $tweet;
 
-}
+  }
 
-function renderTweets(tweets) {
-    tweets.forEach(function(element) {
-        var $tweet = createTweetElement(element);
-        $('section.tweet').append($tweet);
-        });
+  function renderTweets(tweets) {
+    tweets.forEach(function (element) {
+      var $tweet = createTweetElement(element);
+      $('section.tweet').append($tweet);
+    });
+  }
+  // loops through tweets
+  // calls createTweetElement for each tweet
+  // takes return value and appends it to the tweets container
+
+
+
+$('form').on('submit', function (tweet) {
+tweet.preventDefault();
+var data = $('form').serialize();
+
+$.ajax('/tweets/', {
+  method: 'POST',
+  data: data
+}).done(function () {
+  console.log(data)
+  $('form textarea').val('');
+});
+});
+
+function loadTweets() {
+  $.ajax({
+    Method: 'GET',
+    url: '/tweets/',
+    success: function(data) {
+      renderTweets(data)
     }
-    // loops through tweets
-      // calls createTweetElement for each tweet
-      // takes return value and appends it to the tweets container
-  
-renderTweets(data);
+  });
+  console.log(data);
+};
 
+loadTweets();
 });
