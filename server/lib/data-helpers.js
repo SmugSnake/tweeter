3f594d2a42e02448254c7dc1eea68462925033e1
Evan Kerr <evankrkerr90@gmail.com>
2018-06-21 18:27:08 +0000

    switched to MongoDB successfully except for db.close

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -1,27 +1,27 @@
 "use strict";
 
 // Simulates the kind of delay we see with network or filesystem operations
-const simulateDelay = require("./util/simulate-delay");
-
+//const simulateDelay = require("./util/simulate-delay");
+//const tweeter = client.db('tweeter');
 // Defines helper functions for saving and getting tweets, using the database `db`
 module.exports = function makeDataHelpers(db) {
   return {
+    //const tweeter = client.db('tweeter');
 
     // Saves a tweet to `db`
     saveTweet: function(newTweet, callback) {
-      simulateDelay(() => {
-        db.tweets.push(newTweet);
+        db.collection("tweets").insertOne(newTweet);
         callback(null, true);
-      });
     },
 
     // Get all tweets in `db`, sorted by newest first
     getTweets: function(callback) {
-      simulateDelay(() => {
-        const sortNewestFirst = (a, b) => a.created_at - b.created_at;
-        callback(null, db.tweets.sort(sortNewestFirst));
-      });
+      //const sortNewestFirst = (a, b) => a.created_at - b.created_at;
+       db.collection("tweets").find().toArray(callback);
+        //callback(null, tweets.sort(sortNewestFirst));
     }
 
+
   };
+  db.close();
 }
